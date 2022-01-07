64be9930333aa55bbb8e9e56a0cbeee475db0465
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-07 21:52:45 +0000

    .

--- componrnts/nav/Nav.js
+++ componrnts/nav/Nav.js
@@ -2,18 +2,48 @@
 
 import React from "react";
 import styles from "./Nav.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const NavBar = ({ username }) => {
+  const router = useRouter();
+
+  const handleOnCLickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnMyList = (e) => {
+    e.preventDefault();
+    router.push("browse/my-list");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         Logo
-        <a className={styles.logoLink} href='/'>
-          <div className={styles.logoWrapper}>Netflix</div>
-        </a>
+        <Link href='/'>
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>Netflix</div>
+          </a>
+        </Link>
         <ul className={styles.navItems}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem2}>My List</li>
+          <li
+            onClick={(e) => {
+              handleOnCLickHome(e);
+            }}
+            className={styles.navItem}
+          >
+            Home
+          </li>
+          <li
+            onClick={(e) => {
+              handleOnMyList(e);
+            }}
+            className={styles.navItem2}
+          >
+            My List
+          </li>
         </ul>
         <nav className={styles.navContainer}>
           <div>
@@ -23,7 +53,9 @@ const NavBar = ({ username }) => {
             </button>
             <div className={styles.navDropdown}>
               <div>
-                <a className={styles.linkName}>Sign out</a>
+                <Link href='/login'>
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
                 <div className={styles.lineWrapper}></div>
               </div>
             </div>
