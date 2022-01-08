b17084bf8da11abba5a299ab0cbf7d670bc3981f
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-08 21:35:59 +0000

    set up auth with magic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+/** @format */
+
+import "../styles/globals.css";
+import { magic } from "../lib/magic-client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      const LoggedIn = await magic.user.isLoggedIn(); // will return true or false
+
+      console.log("IS LOGGED IN", LoggedIn);
+
+      if (LoggedIn) {
+        router.push("/");
+      } else {
+        router.push("/login");
+      }
+    };
+
+    isLoggedIn();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
