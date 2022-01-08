2fa68a30a40db94a7eee206ba2362764f95f2ea9
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-08 20:34:25 +0000

    set up auth with magic

---
+++ pages/login.js
@@ -0,0 +1,98 @@
+/** @format */
+
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import { magic } from "../lib/magic-client";
+
+import styles from "../styles/Login.module.css";
+const Login = () => {
+  const [userMsg, setUserMsg] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLoginWithEmail = async (e) => {
+    e.preventDefault();
+
+    if (email) {
+      if (email === "kagyeman@ymail.com") {
+        try {
+          setLoading(true);
+          const didToken = await magic.auth.loginWithMagicLink({
+            email: email,
+          });
+
+          console.log({ didToken });
+
+          if (didToken) {
+            router.push("/");
+            setLoading(false);
+          }
+        } catch (error) {
+          // Handle errors if required!
+          console.log("Login error", error);
+          setLoading(false);
+        }
+      }
+    } else {
+      setUserMsg("Enter a valid email address");
+      setLoading(false);
+    }
+  };
+
+  const handleOnChangeEmail = (e) => {
+    e.preventDefault();
+    setUserMsg("");
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Netflix SignIn</title>
+        <meta name='description' content='Signin to Netflix' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src={"/static/netflix.svg"}
+                alt='Netflix Logo'
+                width={128}
+                height={34}
+              />
+            </div>
+          </a>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            className={styles.emailInput}
+            type={"email"}
+            placeholder='Emaill address'
+            value={email}
+            onChange={(e) => handleOnChangeEmail(e)}
+          ></input>
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button
+            className={styles.loginBtn}
+            onClick={(e) => handleLoginWithEmail(e)}
+          >
+            {loading ? "Loading" : "Sign In"}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
