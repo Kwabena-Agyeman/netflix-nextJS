8faa380a4ec140b42fd1d949fa8dde2c0acbc42f
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-08 23:50:23 +0000

    set up auth with magic

---
+++ componrnts/loading/Loading.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import styles from "./loading.module.css";
+
+const Loading = () => {
+  return <p className={styles.loader}>Loading...</p>;
+};
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 /** @format */
 
+import { useState } from "react";
 import "../styles/globals.css";
 import { magic } from "../lib/magic-client";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Loading from "../componrnts/loading/Loading";
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
@@ -22,9 +25,23 @@ function MyApp({ Component, pageProps }) {
     };
 
     isLoggedIn();
+    // eslint-disable-next-line
   }, []);
 
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router.events]);
+
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
 
 export default MyApp;
