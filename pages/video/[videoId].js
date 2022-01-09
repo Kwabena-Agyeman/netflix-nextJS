f0ba8841b175766dec2666519df87e1b75347977
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-09 18:48:16 +0000

    tackling new page video page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -3,6 +3,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
 
 Modal.setAppElement("#__next");
 
@@ -10,10 +11,25 @@ const Video = () => {
   const router = useRouter();
   const videoId = router.query.videoId;
   return (
-    <div>
-      Video Page {videoId}
-      <Modal isOpen={true} contentLabel='Watch the video'>
-        Modal Body
+    <div className={styles.container}>
+      <Modal
+        className={styles.modal}
+        isOpen={true}
+        contentLabel='Watch the video'
+        onRequestClose={() => {
+          router.back();
+        }}
+        overlayClassName={styles.overlay}
+      >
+        <iframe
+          id='ytplayer'
+          className={styles.videoPlayer}
+          type='text/html'
+          width='100%'
+          height='360'
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameBorder='0'
+        ></iframe>
       </Modal>{" "}
     </div>
   );
