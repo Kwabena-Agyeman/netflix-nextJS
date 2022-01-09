62c355504432b09d8f2e5c5486fc2594e1821b55
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-09 19:35:04 +0000

    tackling new page video page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -4,11 +4,54 @@ import React from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
+import clsx from "classnames";
 
 Modal.setAppElement("#__next");
 
-const Video = () => {
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps() {
+  const video = {
+    title: "Breaking Bad",
+    publishTime: "2008-01-25",
+    description: "Story of Walter White",
+    channelTitle: "Netflix",
+    viewCount: 10000,
+  };
+
+  return {
+    props: {
+      video,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 60, // In seconds
+  };
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// the path has not been generated.
+export async function getStaticPaths() {
+  const listOfVideos = ["1JLUn2DFW4w"];
+  // Get the paths we want to pre-render based on posts
+  const paths = listOfVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: blocking } will server-render pages
+  // on-demand if the path doesn't exist.
+  return { paths, fallback: "blocking" };
+}
+
+const Video = ({ video }) => {
   const router = useRouter();
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
   const videoId = router.query.videoId;
   return (
     <div className={styles.container}>
@@ -30,6 +73,25 @@ const Video = () => {
           src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder='0'
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>{" "}
     </div>
   );
