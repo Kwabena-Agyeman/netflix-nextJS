9e60d3be5f4874d4aacd07f5a1c036dd9bc9d51c
Kwabena Agyeman <kagyeman@ymail.com>
2022-01-09 19:52:18 +0000

    tackling new page video page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -5,38 +5,16 @@ import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
 import clsx from "classnames";
+import { getYoutubeVideoById } from "../../lib/videos";
 
 Modal.setAppElement("#__next");
-
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const video = {
-    title: "Breaking Bad",
-    publishTime: "2008-01-25",
-    description: "Story of Walter White",
-    channelTitle: "Netflix",
-    viewCount: 10000,
-  };
-
-  return {
-    props: {
-      video,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 60, // In seconds
-  };
-}
 
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // the path has not been generated.
 export async function getStaticPaths() {
   const listOfVideos = ["1JLUn2DFW4w"];
-  // Get the paths we want to pre-render based on posts
+  // Get the paths we want to pre-render based on videos
   const paths = listOfVideos.map((videoId) => ({
     params: { videoId },
   }));
@@ -47,12 +25,37 @@ export async function getStaticPaths() {
   return { paths, fallback: "blocking" };
 }
 
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps(context) {
+  const videoId = context.params.videoId; // getting the videoID from the paths created my getStaticPaths function. All the params in context are the params we passed as paths in getStaticPaths
+  const videoArray = await getYoutubeVideoById(videoId);
+
+  return {
+    props: {
+      //pass the vidoes as props to the page
+      video: videoArray.length > 0 ? videoArray[0] : {},
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 60, // In seconds
+  };
+}
+
 const Video = ({ video }) => {
   const router = useRouter();
-
-  const { title, publishTime, description, channelTitle, viewCount } = video;
-
   const videoId = router.query.videoId;
+
+  const {
+    title,
+    publishTime,
+    description,
+    channelTitle,
+    statistics: { viewCount } = { viewCount: 0 },
+  } = video;
+
   return (
     <div className={styles.container}>
       <Modal
